45455edf189810d0cfdbdc095b3e81b5aa9533af
sun_root <94284939+kug4586@users.noreply.github.com>
2023-08-29 21:51:16 +0900

    correcting files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-///
-/// 메인페이지
-/// - (앱의 상단 메뉴 : 메인 페이지) = 1 : 12
-/// - 위젯은 모두 같은 크기
-///
+import 'package:aiia/config.dart';
+import 'package:aiia/main_page/layout.dart';
 
 import 'package:flutter/material.dart';
 
@@ -16,116 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "무한길잡이",
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(),
-        child: SafeArea(child: Column(
-          children: [
-            // 상단 메뉴
-            Flexible(
-              flex: 1,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 1,
-                      color: Colors.black
-                    )
-                  ),
-                  color: Colors.grey.shade300
-                ),
-                child: Center(child: Text(
-                  "상단 메뉴",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16
-                  )
-                ))
-              )
-            ),
-            // 메인 페이지
-            Flexible(
-              flex: 12,
-              child: Container(
-                child: Column(children: [
-                  // My Grade 위젯
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
-                                      width: 1,
-                                      color: Colors.black
-                                  )
-                              )
-                          ),
-                          child: Center(child: Text(
-                              "My Grade",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16
-                              )
-                          ))
-                      )
-                  ),
-                  // 입시 결과표 위젯
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
-                                      width: 1,
-                                      color: Colors.black
-                                  )
-                              )
-                          ),
-                          child: Center(child: Text(
-                              "입시 결과표",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16
-                              )
-                          ))
-                      )
-                  ),
-                  // 표 생성 위젯
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                          child: Center(child: Text(
-                              "표 생성",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16
-                              )
-                          ))
-                      )
-                  ),
-                ])
-              ),
-            )
-          ]
-        ))
-      )
+      theme: ThemeData(fontFamily: font_family),
+      title: "무한길잡이",
+      home: HomeLayout(),
     );
   }
 }
