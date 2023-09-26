a1effe85ef6275f760e67f16e86a45cf3abeedf7
kug4586 <kug040202@gmail.com>
2023-09-26 19:59:44 +0900

    [feat] 애니메이션 추가

--- lib/config/animation.dart
+++ lib/config/animation.dart
@@ -1,1 +1,43 @@
+///
+/// 애니메이션 폴더
+///
+
 import 'package:flutter/material.dart';
+
+Widget slideLeft2Right(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child
+    ) {
+  return SlideTransition(
+    child: child,
+    position: animation.drive(
+      Tween(
+        begin: Offset(-1.0, 0.0),
+        end: Offset.zero
+      ).chain(
+        CurveTween(curve: Curves.ease)
+      )
+    )
+  );
+}
+
+Widget slideRigth2Left(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child
+    ) {
+  return SlideTransition(
+      child: child,
+      position: animation.drive(
+          Tween(
+              begin: Offset(1.0, 0.0),
+              end: Offset.zero
+          ).chain(
+              CurveTween(curve: Curves.ease)
+          )
+      )
+  );
+}
